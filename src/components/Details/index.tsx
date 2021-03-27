9f4456fa09a733821d77d42306b5a659a236a583
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-03-27 00:26:28 -0300

    feito estilizacao

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,41 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MovieDetails } from "../../interfaces";
+import { getActorsLinks } from "../../services/api";
+import styles from "./details.module.css";
 
 import { Container, LeftSide, RightSide, Info } from "./styles";
 
 const Details: React.FC<{ movie: MovieDetails }> = ({ movie }) => {
+  const resp = movie.Ratings.find(({ Source }) => Source === "Metacritic");
+  let rate = "0";
+  if (resp) {
+    rate = resp.Value.split("/")[0];
+  }
+  const [images, setImages] = useState([]);
+  const handleImagesRequest = async () => {
+    const resp = await getActorsLinks(movie.Actors.split(","));
+    setImages(resp);
+  };
+  useEffect(() => {
+    handleImagesRequest();
+  }, []);
   return (
-    <Container>
-      <LeftSide>
+    <div className={styles.Container}>
+      <div className={styles.LeftSide}>
         <img src={movie.Poster}></img>
-        <span>{movie.Title}</span>
-        <span>{movie.Director}</span>
+        <h1>{movie.Title}</h1>
+        <section>
+          <span>Director: </span>
+          <span style={{ color: "#7FA6C2" }}>{movie.Director}</span>
+        </section>
         <span>{movie.Genre}</span>
-      </LeftSide>
-      <RightSide>
+      </div>
+      <div className={styles.RightSide}>
+        <h1 className={styles.Title}>Sinopse</h1>
         <span>{movie.Plot}</span>
-        <Info>
+        <article className={styles.Info}>
           <h3>Rated</h3>
           <span>{movie.Rated}</span>
-        </Info>
-        <Info>
+        </article>
+        <article className={styles.Info}>
           <h3>Website</h3>
           <span>{movie.Website}</span>
-        </Info>
-        <Info>
+        </article>
+        <article className={styles.Info}>
           <h3>Genre</h3>
           <span>{movie.Genre}</span>
-        </Info>
-        <Info>
+        </article>
+        <article className={styles.Info}>
           <h3>Language</h3>
           <span>{movie.Language}</span>
-        </Info>
-        <Info>
+        </article>
+        <article className={styles.Info}>
           <h3>Year</h3>
           <span>{movie.Year}</span>
-        </Info>
-      </RightSide>
-    </Container>
+        </article>
+        <section className={styles.GridActorsRate}>
+          <aside className={styles.Aside}>
+            <h1 className={styles.Title}>Actors</h1>
+            <div className={styles.ActorSection}>
+              {images.map((link, index) => (
+                <div className={styles.ActorWrapper} key={index}>
+                  <img className={styles.ActorImg} src={link} alt="actor" />
+                  <span>{movie.Actors.split(",")[index]}</span>
+                </div>
+              ))}
+            </div>
+          </aside>
+          <aside>
+            <h1 className={styles.Title}>Metacritics</h1>
+            <div>
+              <div className={styles.RateBar}>
+                {console.log(rate)}
+                {resp ? (
+                  <div>
+                    <div
+                      className={styles.Rate}
+                      style={{
+                        width: `${parseInt(rate) * 2}px`,
+                      }}
+                    />
+                    <span className={styles.RateInfo}>{rate}%</span>
+                  </div>
+                ) : (
+                  <span className={styles.RateInfo}>No Rate</span>
+                )}
+              </div>
+            </div>
+          </aside>
+        </section>
+      </div>
+    </div>
   );
 };
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { MovieDetails } from "../interfaces";
 import key from "./key";
 
 export async function searchByQuery(movie: string) {
@@ -10,7 +11,9 @@ export async function searchByQuery(movie: string) {
     });
 }
 
-export async function filmDetails(id: string |string[]) {
+export async function filmDetails(
+  id: string | string[]
+): Promise<MovieDetails> {
   return axios
     .get(`http://www.omdbapi.com/?i=${id}&apikey=${key}`)
     .then((r) => r.data)
@@ -22,4 +25,21 @@ export async function searchRandom() {
     console.log(r.status);
     return r.request.response;
   });
+}
+
+async function getActorImageByName(ActorName: string): Promise<string> {
+  return axios
+    .get(`https://images-web-scrapper-1212.herokuapp.com/${ActorName}`)
+    .then((r) => r.data.resp)
+    .catch((deupau) => deupau);
+}
+
+export async function getActorsLinks(ActorsName: string[]): Promise<string[]> {
+  const allLinks = [];
+  for (let i = 0; i < ActorsName.length; i++) {
+    const links = await getActorImageByName(ActorsName[i]);
+    allLinks.push(links[1]);
+  }
+
+  return allLinks;
 }
